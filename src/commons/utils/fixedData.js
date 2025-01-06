6b1276558cacdc6522df4065087bc6fcb53db881
Ignacio Ballester <front.developer.iba@gmail.com>
2025-01-06 14:44:24 +0100

    feat: añadido informacion fija (apliado info vida y ministerio)

---
+++ src/commons/utils/fixedData.js
@@ -0,0 +1,770 @@
+export const meeting = [
+  {
+    id: "01",
+    txt: "Vida y Ministerio",
+  },
+  {
+    id: "02",
+    txt: "Reunión Publica",
+  },
+  {
+    id: "03",
+    txt: "Responsabilidad",
+  },
+  {
+    id: "02",
+    txt: "Visita Siperintendente",
+  },
+];
+export const sections = [
+  {
+    idr: "01",
+    ids: "01",
+    txt: "Tesoros",
+  },
+  {
+    idr: "01",
+    ids: "02",
+    txt: "Seamos Mejores Maestros",
+  },
+  {
+    idr: "01",
+    ids: "03",
+    txt: "Nuetro Vida Cristiana",
+  },
+  {
+    idr: "01",
+    ids: "04",
+    txt: "Presidente",
+  },
+  {
+    idr: "01",
+    ids: "05",
+    txt: "Oracion",
+  },
+  {
+    idr: "02",
+    ids: "01",
+    txt: "Presidencia",
+  },
+  {
+    idr: "02",
+    ids: "02",
+    txt: "Oracion",
+  },
+  {
+    idr: "02",
+    ids: "03",
+    txt: "Orador",
+  },
+  {
+    idr: "02",
+    ids: "04",
+    txt: "Lector",
+  },
+  {
+    idr: "03",
+    ids: "01",
+    txt: "Sonido",
+  },
+  {
+    idr: "03",
+    ids: "02",
+    txt: "Microfonos",
+  },
+  {
+    idr: "03",
+    ids: "03",
+    txt: "Acomodador",
+  },
+  {
+    idr: "03",
+    ids: "04",
+    txt: "Limpieza",
+  },
+  {
+    idr: "03",
+    ids: "05",
+    txt: "Emblemas",
+  },
+];
+export const vidaMinister = [
+  {
+    id: 1,
+    franja: "30/12-5/01",
+    sections: [
+      {
+        id: "01",
+        parts: [
+          {
+            idasig: 1,
+            time: 10,
+            title: "Sembraron con lágrimas, pero cosecharon con alegria",
+            base: "Salmos 126: 1-3,5,6",
+          },
+          {
+            idasig: 2,
+            time: 10,
+            title: "Busquemos perlas escondidas",
+            base: "Salmos 120-126",
+          },
+          {
+            idasig: 3,
+            time: 4,
+            title: "Lectura de la Biblia",
+            base: "Salmos 124:1-126:6",
+            punt: "th lección 5",
+          },
+        ],
+      },
+      {
+        id: "02",
+        parts: [
+          {
+            idasig: 4,
+            time: 3,
+            title: "Empiece conversaciones",
+            base: "PREDICACIÓN PÚBLICA.",
+            punt: "lmd lección 3 punto 5",
+          },
+          {
+            idasig: 5,
+            time: 4,
+            title: "Haga revisitas",
+            base: "DE CASA EN CASA. En un visita anterior, la persona le dijo que no tenía claro si se puede confiar en la Biblia.",
+            punt: "lmd lección 9 punto 5",
+          },
+          {
+            idasig: 6,
+            time: 5,
+            title: "Haga discíulos",
+            base: "lff lección 16 introducción y puntos 1-3.",
+            punto: "lmd lección 11 punto 3",
+          },
+        ],
+      },
+      {
+        id: "03",
+        parts: [
+          {
+            idasig: 7,
+            time: 15,
+            title: "Disfrute pensando en las promesas de Dios",
+            base: "ver hoja de vida y ministerio",
+            punt: "Analisis con el auditorio",
+          },
+          {
+            idasig: 8,
+            time: 30,
+            title: "Estudio Bíblico de la congragación",
+            base: "bt cap. 20 párrs. 8-12 y recuadro de la pág. 161.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    franja: "6/01-12/01",
+    sections: [
+      {
+        id: "01",
+        parts: [
+          {
+            idasig: 1,
+            time: 10,
+            title: "Padres, sigan cuidando la herencia que jehová les dio.",
+            base: "Salmos 127: 1,2,3,4",
+          },
+          {
+            idasig: 2,
+            time: 10,
+            title: "Busquemos perlas escondidas",
+            base: "Salmos 127-134",
+          },
+          {
+            idasig: 3,
+            time: 4,
+            title: "Lectura de la Biblia",
+            base: "Salmos 132:1-18",
+            punt: "th lección 2",
+          },
+        ],
+      },
+      {
+        id: "02",
+        parts: [
+          {
+            idasig: 4,
+            time: 3,
+            title: "Empiece conversaciones",
+            base: "PREDICACIÓN INFORMAL.",
+            punt: "lmd lección 1 punto 3",
+          },
+          {
+            idasig: 5,
+            time: 4,
+            title: "Haga revisitas",
+            base: "PREDICACIÓN INFORMAL. La persona dice algo que no está de acuerdo con lo que enseña la Biblia.",
+            punt: "lmd lección 5 punto 4",
+          },
+          {
+            idasig: 6,
+            time: 5,
+            title: "Haga discíulos",
+            base: "lff lección 16 puntos 4,5.",
+            punto: "lmd lección 10 punto 4",
+          },
+        ],
+      },
+      {
+        id: "03",
+        parts: [
+          {
+            idasig: 7,
+            time: 15,
+            title: "Padres, ¿están usando esta herramienta tan potente?",
+            base: "ver hoja de vida y ministerio",
+            punt: "Analisis con el auditorio",
+          },
+          {
+            idasig: 8,
+            time: 30,
+            title: "Estudio Bíblico de la congragación",
+            base: "bt cap. 20 párrs. 13-20.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    franja: "13/01-19/01",
+    sections: [
+      {
+        id: "01",
+        parts: [
+          {
+            idasig: 1,
+            time: 10,
+            title: "Nuestro Señor es más grande que todos los demás dises.",
+            base: "Salmos 135: 5,6,14; 136:23 y Ex 14:29-31",
+          },
+          {
+            idasig: 2,
+            time: 10,
+            title: "Busquemos perlas escondidas",
+            base: "Salmos 135-137",
+          },
+          {
+            idasig: 3,
+            time: 4,
+            title: "Lectura de la Biblia",
+            base: "Salmos 135:1-21",
+            punt: "th lección 11",
+          },
+        ],
+      },
+      {
+        id: "02",
+        parts: [
+          {
+            idasig: 4,
+            time: 3,
+            title: "Empiece conversaciones",
+            base: "PREDICACIÓN INFORMAL. La persona muestra interés y se intercambian los datos de contacto.",
+            punt: "lmd lección 2 punto 4",
+          },
+          {
+            idasig: 5,
+            time: 4,
+            title: "Haga revisitas",
+            base: "DE CASA EN CASA. Invite a la persona a una reunión.",
+            punt: "lmd lección 9 punto 4",
+          },
+          {
+            idasig: 6,
+            time: 5,
+            title: "Explique sus creencias",
+            base: "Escenificación. ijwfq artículo 7. Tema: ¿Son cristianos los testigos de jehová?",
+            punto: "th lección 12",
+          },
+        ],
+      },
+      {
+        id: "03",
+        parts: [
+          {
+            idasig: 7,
+            time: 15,
+            title: "Nesesidades de la congregación",
+            base: "",
+            punt: "",
+          },
+          {
+            idasig: 8,
+            time: 30,
+            title: "Estudio Bíblico de la congragación",
+            base: "bt cap. 21 párrs. 1-7 y recuadro de la pág. 166.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    franja: "20/01-26/01",
+    sections: [
+      {
+        id: "01",
+        parts: [
+          {
+            idasig: 1,
+            time: 10,
+            title: "¡Que los nervios no lo frenen!",
+            base: "Salmos 138: 1,3,6",
+          },
+          {
+            idasig: 2,
+            time: 10,
+            title: "Busquemos perlas escondidas",
+            base: "Salmos 138, 139",
+          },
+          {
+            idasig: 3,
+            time: 4,
+            title: "Lectura de la Biblia",
+            base: "Salmos 139:1-18",
+            punt: "th lección 2",
+          },
+        ],
+      },
+      {
+        id: "02",
+        parts: [
+          {
+            idasig: 4,
+            time: 3,
+            title: "Empiece conversaciones",
+            base: "PREDICACIÓN PÚBLICA.",
+            punt: "lmd lección 2 punto 3",
+          },
+          {
+            idasig: 5,
+            time: 4,
+            title: "Haga discípulos",
+            base: "PREDICACIÓN PÚBLICA. Ofrézcale a la persona un curso de la Biblia y muéstrele cómo son nuestras clases.",
+            punt: "lmd lección 10 punto 3",
+          },
+          {
+            idasig: 6,
+            time: 5,
+            title: "Discurso",
+            base: "ijwyp artículo 105. Título: ¿Qué me puede ayudar si soy muy tímido?",
+            punto: "th lección 16",
+          },
+        ],
+      },
+      {
+        id: "03",
+        parts: [
+          {
+            idasig: 7,
+            time: 15,
+            title: "Aunque sea tímido, puede tener éxito sirviendo a Jehová",
+            base: "ver hoja de vida y ministerio",
+            punt: "Analisis con el auditorio",
+          },
+          {
+            idasig: 8,
+            time: 30,
+            title: "Estudio Bíblico de la congragación",
+            base: "bt cap. 21 párrs. 8-13 y recuadro de la pág. 169.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 5,
+    franja: "27/01-2/02",
+    sections: [
+      {
+        id: "01",
+        parts: [
+          {
+            idasig: 1,
+            time: 10,
+            title: "¿Qué hará después de orar?",
+            base: "Salmos 141: 5; 143: 5, 10",
+          },
+          {
+            idasig: 2,
+            time: 10,
+            title: "Busquemos perlas escondidas",
+            base: "Salmos 140-143",
+          },
+          {
+            idasig: 3,
+            time: 4,
+            title: "Lectura de la Biblia",
+            base: "Salmos 141:1-10",
+            punt: "th lección 12",
+          },
+        ],
+      },
+      {
+        id: "02",
+        parts: [
+          {
+            idasig: 4,
+            time: 4,
+            title: "Empiece conversaciones",
+            base: "PREDICACIÓN INFORMAL. Después de ayudar a la persona de alguna manera, dirija la conversación a un tema bíblico.",
+            punt: "lmd lección 3 punto 5",
+          },
+          {
+            idasig: 5,
+            time: 3,
+            title: "Haga revisitas",
+            base: "PREDICACIÓN PÚBLICA. La persona le dice que está ocupada.",
+            punt: "lmd lección 7 punto 3",
+          },
+          {
+            idasig: 6,
+            time: 5,
+            title: "Explique sus creencias",
+            base: "Escenificación. ijwfq artículo 21. Tema: ¿Por qué no aceptan transfusiones de sangre los testigos de Jehová?",
+            punto: "th lección 7",
+          },
+        ],
+      },
+      {
+        id: "03",
+        parts: [
+          {
+            idasig: 7,
+            time: 15,
+            title:
+              "Esté preparado por si necesita atención médica o una intercención quirúrgica.",
+            base: "ver hoja de vida y ministerio",
+            punt: "Analisis con el auditorio",
+          },
+          {
+            idasig: 8,
+            time: 30,
+            title: "Estudio Bíblico de la congragación",
+            base: "bt cap. 21 párrs. 14-22.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 6,
+    franja: "3/02-9/02",
+    sections: [
+      {
+        id: "01",
+        parts: [
+          {
+            idasig: 1,
+            time: 10,
+            title: "¡Feliz el pueblo que tiene por Dios a Jehová",
+            base: "Salmos 144: 11-15; 146: 5, 10",
+          },
+          {
+            idasig: 2,
+            time: 10,
+            title: "Busquemos perlas escondidas",
+            base: "Salmos 144-146",
+          },
+          {
+            idasig: 3,
+            time: 4,
+            title: "Lectura de la Biblia",
+            base: "Salmos 144:1-15",
+            punt: "th lección 11",
+          },
+        ],
+      },
+      {
+        id: "02",
+        parts: [
+          {
+            idasig: 4,
+            time: 4,
+            title: "Empiece conversaciones",
+            base: "DE CASA EN CASA. La persona le dice que está estudiando en la universidad.",
+            punt: "lmd lección 1 punto 5",
+          },
+          {
+            idasig: 5,
+            time: 4,
+            title: "Haga revisitas",
+            base: "PREDICACIÓN INFORMAL. Presente y analice un video del kit de enseñanza, pero no lo ponga.",
+            punt: "lmd lección 7 punto 4",
+          },
+          {
+            idasig: 6,
+            time: 4,
+            title: "Discurso",
+            base: "lmd apéndice A punto 7. Título: La esposa debería respetar profundamente a su esposo.",
+            punto: "th lección 1",
+          },
+        ],
+      },
+      {
+        id: "03",
+        parts: [
+          {
+            idasig: 7,
+            time: 10,
+            title: "Jehová quiere que usted sea feliz.",
+            base: "ver hoja de vida y ministerio",
+            punt: "Analisis con el auditorio",
+          },
+          {
+            idasig: 8,
+            time: 5,
+            title: "Necesidades de la congregación.",
+          },
+          {
+            idasig: 9,
+            time: 30,
+            title: "Estudio Bíblico de la congragación",
+            base: "bt cap. 22 párrs. 1-6.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 7,
+    franja: "10/02-16/02",
+    sections: [
+      {
+        id: "01",
+        parts: [
+          {
+            idasig: 1,
+            time: 10,
+            title: "Tenemos muchas razones para alabar a Jah",
+            base: "Salmos 147: 3,4,5,19,20",
+          },
+          {
+            idasig: 2,
+            time: 10,
+            title: "Busquemos perlas escondidas",
+            base: "Salmos 147-150",
+          },
+          {
+            idasig: 3,
+            time: 4,
+            title: "Lectura de la Biblia",
+            base: "Salmos 148:1-149:9",
+            punt: "th lección 11",
+          },
+        ],
+      },
+      {
+        id: "02",
+        parts: [
+          {
+            idasig: 4,
+            time: 3,
+            title: "Empiece conversaciones",
+            base: "DE CASA EN CASA. La persona le dice que tiene una enfermedad crónica.",
+            punt: "lmd lección 2 punto 5",
+          },
+          {
+            idasig: 5,
+            time: 4,
+            title: "Empiece conversaciones",
+            base: "PREDICACIÓN INFORMAL. Busque la forma de contarle a la persona algo que aprendió hace poco en una reunión.",
+            punt: "lmd lección 4 punto 3",
+          },
+          {
+            idasig: 6,
+            time: 5,
+            title: "Discurso",
+            base: "W19.03 10 párrs 7-11. Título: Escuche a Jesús: Predique las buenas noticias. Durante el discurso, use la imagen",
+            punto: "th lección 14",
+          },
+        ],
+      },
+      {
+        id: "03",
+        parts: [
+          {
+            idasig: 7,
+            time: 15,
+            title: "Informe de servicio anual.",
+            base: "ver hoja de vida y ministerio",
+            punt: "Analisis con el auditorio",
+          },
+          {
+            idasig: 8,
+            time: 30,
+            title: "Estudio Bíblico de la congragación",
+            base: "bt cap. 22 párrs. 7-14 y recudros de las págs. 174,177.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 8,
+    franja: "17/02-23/02",
+    sections: [
+      {
+        id: "01",
+        parts: [
+          {
+            idasig: 1,
+            time: 10,
+            title: "Joven, ¿a quién escucharás?",
+            base: "Proverbios 1:8, 10, 15",
+          },
+          {
+            idasig: 2,
+            time: 10,
+            title: "Busquemos perlas escondidas",
+            base: "Proverbios 1",
+          },
+          {
+            idasig: 3,
+            time: 4,
+            title: "Lectura de la Biblia",
+            base: "Proverbios 1:1-23",
+            punt: "th lección 10",
+          },
+        ],
+      },
+      {
+        id: "02",
+        parts: [
+          {
+            idasig: 4,
+            time: 2,
+            title: "Empiece conversaciones",
+            base: "PREDICACIÓN PÚBLICA. La persona quiere discutir con usted.",
+            punt: "lmd lección 6 punto 5",
+          },
+          {
+            idasig: 5,
+            time: 2,
+            title: "Empiece conversaciones",
+            base: "PREDICACIÓN PÚBLICA. La persona muestra interés y se intercambian los datos de contacto.",
+            punt: "lmd lección 1 punto 5",
+          },
+          {
+            idasig: 6,
+            time: 2,
+            title: "Empiece conversaciones",
+            base: "PREDICACIÓN INFORMAL. Háblele a la persona sobre nuestros cursos bíblicos y entréguele una tarjeta de contacto de nuestras clases de la Biblia.",
+            punt: "lmd lección 9 punto 5",
+          },
+          {
+            idasig: 7,
+            time: 5,
+            title: "Haga discípulos",
+            base: "lff lección 16 punto 6. El estudiante se pregunta si los milagros de Jesús realmente sucedieron. Utilice un artículo de la sección 'Descubra algo más' para ayudarlo.",
+            punto: "th lección 3",
+          },
+        ],
+      },
+      {
+        id: "03",
+        parts: [
+          {
+            idasig: 8,
+            time: 15,
+            title: "Necesidades de la congregación.",
+          },
+          {
+            idasig: 9,
+            time: 30,
+            title: "Estudio Bíblico de la congragación",
+            base: "bt cap. 22 párrs. 15-21.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 9,
+    franja: "24/02-2/03",
+    sections: [
+      {
+        id: "01",
+        parts: [
+          {
+            idasig: 1,
+            time: 10,
+            title: "Por qué estudiar con ganas.",
+            base: "Proverbios 2:3,4,5-7,11,12",
+          },
+          {
+            idasig: 2,
+            time: 10,
+            title: "Busquemos perlas escondidas",
+            base: "Proverbios 2",
+          },
+          {
+            idasig: 3,
+            time: 4,
+            title: "Lectura de la Biblia",
+            base: "Proverbios 2:1-22",
+            punt: "th lección 12",
+          },
+        ],
+      },
+      {
+        id: "02",
+        parts: [
+          {
+            idasig: 4,
+            time: 4,
+            title: "Empiece conversaciones",
+            base: "DE CASA EN CASA. Muéstrele a una persona cómo encontrar en jw.org información útil para los matrimonios.",
+            punt: "lmd lección 1 punto 3",
+          },
+          {
+            idasig: 5,
+            time: 3,
+            title: "Haga revisitas",
+            base: "PREDICACIÓN INFORMAL. En una visita anterior, la persona mostró interés sobre un tema. Ofrézcale una revista que hable de ese tema.",
+            punt: "lmd lección 9 punto 3",
+          },
+          {
+            idasig: 6,
+            time: 5,
+            title: "Discurso",
+            base: "lmd apéndice A punto 8. Título: El esposo y la esposa deberían ser fieles el uno al otro.",
+            punto: "th lección 13",
+          },
+        ],
+      },
+      {
+        id: "03",
+        parts: [
+          {
+            idasig: 7,
+            time: 15,
+            title: "¿Eres un cazatesoros?",
+            base: "ver hoja de vida y ministerio",
+            punt: "Analisis con el auditorio",
+          },
+          {
+            idasig: 8,
+            time: 30,
+            title: "Estudio Bíblico de la congragación",
+            base: "bt cap. 23 párrs. 1-8 e introducción a la seccion 8.",
+          },
+        ],
+      },
+    ],
+  },
+];
+
